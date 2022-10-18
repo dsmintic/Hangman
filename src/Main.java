4c4c56b0d8a18c4ca070d2a8e457e3a4caf51bb5
Damir Smintic <dsmintic@gmail.com>
2022-10-18 09:35:17 +0200

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+//       TO DO:
+//       1. WORDS LIST - String Array
+//       2. Bad guesses - int
+//       3. Random choose a word from list
+//       4. Do char Array for correst and incorect letters
+//       5. Do main loop ( hmmm while or do/while - need to think)
+//          5.1 Hangman picture
+//          5.2
+
+
+    }
+}
