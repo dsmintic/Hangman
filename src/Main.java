85e2ed5c215166bcbcce9897cf20836568538241
Damir Smintic <dsmintic@gmail.com>
2022-10-20 15:24:46 +0200

    8rd - final demo

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,86 @@
 import java.io.CharArrayReader;
 import java.util.*;
+import static java.lang.System.exit;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        Random rand = new Random();
 
         int userInput;
         int badGuessesCount = 0;
         int maxBadGuesses = 6;
 
-        ArrayList<String> words = new ArrayList<String>();
-        words.add("cat");
-        words.add("dog");
-        words.add("java");
-        words.add("fish");
-        words.add("apple");
-        words.add("boots");
-        words.add("laptop");
-        words.add("class");
-        words.add("cake");
-        words.add("bread");
-
-        String randomWord = words.get(rand.nextInt(words.size()));
-
-        System.out.println("\nWelcome to the game console");
-        System.out.println("Choose an option");
+        System.out.println("\n************************************************");
+        System.out.println("          Welcome to the game console");
+        System.out.println("************************************************");
+        System.out.println("\n               Choose an option");
+        System.out.println("------------------------------------------------");
         System.out.println("1 - Start new Hangman game");
-        System.out.println("2 - Exit game");
+        System.out.println("2 - Exit game console");
         System.out.println("------------------------------------------------");
         userInput = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println(randomWord);
-
-
         if (userInput == 1) {
-            System.out.println("************************************************");
+            System.out.println("\n************************************************");
             System.out.println("Guess the first letter of the word. Good luck :)");
             System.out.println("************************************************");
+            System.out.println("\n               Choose a category");
+            System.out.println("------------------------------------------------");
+
+            do {
+                System.out.println("\n************************************************");
+                System.out.println("Choose the category of words you want to guess");
+                System.out.println("************************************************");
+                System.out.println("1 - Animals");
+                System.out.println("2 - Country");
+                System.out.println("3 - Food");
+                System.out.println("------------------------------------------------");
+                userInput = scanner.nextInt();
+                scanner.nextLine();
+                if (userInput == 1) {
+                    System.out.println("================================================");
+                    System.out.println("                      Animals");
+                    System.out.println("================================================");
+                    Words.setRandomWord(Words.generateAnimal());
+                    break;
+                }
+                else if (userInput == 2) {
+                    System.out.println("===================================================");
+                    System.out.println("                      Country");
+                    System.out.println("===================================================");
+//                    System.out.println("Give me the number of letters for the word (4 - 11)");
+//                    System.out.println("---------------------------------------------------");
+//                    int numLetters = scanner.nextInt();
+//                  canner.nextLine();
+//                  int randomWordLength;
+//                  Random Word Length = User input
+//                  Words.setRandomWord(Words.generateCountry(Random Word Length));
+//
+                    Words.setRandomWord(Words.generateCountry());
+//                    Words.setRandomWord(Words.generateCountry(numLetters));
+                    break;
+                }
+                else if (userInput == 3) {
+                    System.out.println("================================================");
+                    System.out.println("                      Food");
+                    System.out.println("================================================");
+                    Words.setRandomWord(Words.generateFood());
+                    break;
+                }
+            } while (true);
+
+        } else if (userInput >= 2){
+            System.out.println("\n************************************************");
+            System.out.println("Thanks for playing :)");
+            System.out.println("Terminating game console ...");
+            System.out.println("************************************************");
+            exit(0);
         }
+
+        String randomWord = Words.getRandomWord();
+        System.out.println(Words.getRandomWord());
 
         ArrayList<Character> usedLetter = new ArrayList<>();
 
@@ -52,7 +92,7 @@ public class Main {
         }
         //Converts the letters of a random word in an ArrayList I can count wrong guesses
         ArrayList<Character> letters = new ArrayList<>();
-        for (int i = 0; i <randomWord.length(); i++){
+        for (int i = 0; i < randomWord.length(); i++) {
             letters.add(randomWord.charAt(i));
         }
 
@@ -72,9 +112,9 @@ public class Main {
             userLetter = userLetter.toLowerCase();
 
 
-            if (usedLetter.contains(userLetter.charAt(0))){
+            if (usedLetter.contains(userLetter.charAt(0))) {
                 System.out.println("------------------------------------------------");
-                System.out.println("You already used that letter.");
+                System.out.println("\"You already used that letter.\"");
             } else {
                 usedLetter.add(userLetter.charAt(0));
 
@@ -87,7 +127,7 @@ public class Main {
 
                 if (letterFound == false) {
                     System.out.println("------------------------------------------------");
-                    System.out.println("You chose the wrong letter. Please try again.");
+                    System.out.println("\"You chose the wrong letter. Please try again.\"");
                     System.out.println("------------------------------------------------");
                 }
                 if (!letters.contains(userLetter.charAt(0))) {
@@ -105,49 +145,53 @@ public class Main {
             System.out.println();
 
             if (correctLetters == randomWord.length()) {
-                System.out.println("\nCongratulations! You won the game!");
+                System.out.println("*********************************************************");
+                System.out.println("         Congratulations! You won the game!");
+                System.out.println("*********************************************************");
                 break;
             }
 
             if (maxBadGuesses == badGuessesCount) {
                 printHangman(badGuessesCount);
                 System.out.println("*********************************************************");
-                System.out.println("You used 6 attempts. Unfortunately you lost the game :(");
+                System.out.println("\"You used 6 attempts. Unfortunately you lost the game :(\"");
                 System.out.println("*********************************************************");
                 break;
             }
             Collections.sort(usedLetter);
-            System.out.println("\nSo far you have used these letters: " + usedLetter.toString().replace("[","").replace("]",""));
+            System.out.println("\nSo far you have used these letters: " + usedLetter.toString().replace("[", "").replace("]", ""));
         } while (true);
+
     }
 
-    public static void printHangman(int badGuessesCount){
 
-        System.out.println();
-        System.out.println("-------");
-        System.out.println("      |");
-        if (badGuessesCount >= 1) {
-            System.out.println("      0");
-        }
-        if (badGuessesCount >= 2) {
-            System.out.print("     \\");
-            if (badGuessesCount >= 3) {
-                System.out.println(" /");
-            } else {
-                System.out.println();
-            }
-        }
-        if (badGuessesCount >= 4) {
+        private static void printHangman (int badGuessesCount){
+
+            System.out.println();
+            System.out.println("-------");
             System.out.println("      |");
-        }
-        if (badGuessesCount >= 5) {
-            System.out.print("     /");
-            if (badGuessesCount >= 6) {
-                System.out.println(" \\");
-            } else {
-                System.out.println();
+            if (badGuessesCount >= 1) {
+                System.out.println("      0");
             }
+            if (badGuessesCount >= 2) {
+                System.out.print("     \\");
+                if (badGuessesCount >= 3) {
+                    System.out.println(" /");
+                } else {
+                    System.out.println();
+                }
+            }
+            if (badGuessesCount >= 4) {
+                System.out.println("      |");
+            }
+            if (badGuessesCount >= 5) {
+                System.out.print("     /");
+                if (badGuessesCount >= 6) {
+                    System.out.println(" \\");
+                } else {
+                    System.out.println();
+                }
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
 }
--- src/Words.java
+++ src/Words.java
@@ -1,19 +1,84 @@
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Words {
+    private static ArrayList<String> words = new ArrayList<String>();
+    private static String randomWord;
+    private static int numLetters;
 
-    private String words;
-
-    public Words(String words) {
-        this.words = words;
+    public static String generateAnimal(){
+        Random rand = new Random();
+        words.add("cat");
+        words.add("dog");
+        words.add("fish");
+        words.add("elephant");
+        words.add("giraffe");
+        String randomWord = words.get(rand.nextInt(words.size()));
+        return randomWord;
     }
 
-    public String getWords() {
-        return words;
+//    public static String generateCountry(int numLetters){
+        public static String generateCountry(){
+        Random rand = new Random();
+
+        words.add("iran");
+        words.add("iraq");
+        words.add("mali");
+        words.add("china");
+        words.add("egypt");
+        words.add("gabon");
+        words.add("nepal");
+        words.add("france");
+        words.add("denmark");
+        words.add("belgium");
+        words.add("canada");
+        words.add("finland");
+        words.add("pakistan");
+        words.add("columbia");
+        words.add("portugal");
+        words.add("venezuela");
+        words.add("uzbekistan");
+        words.add("afghanistan");
+
+//        for (){
+//        if (numLetters == randomWord.length()) {
+            String randomWord = words.get(rand.nextInt(words.size()));
+//        }
+//        if (){
+//        }
+//        }
+
+        return randomWord;
     }
 
-    public void setWords(String words) {
-        this.words = words;
+    public static String generateFood(){
+        Random rand = new Random();
+        words.add("carrot");
+        words.add("cheese");
+        words.add("cake");
+        words.add("bread");
+        words.add("apple");
+
+        if (numLetters == words.size()) {
+            String randomWord = words.get(rand.nextInt(words.size()));
+        }
+        return randomWord;
     }
 
+    public static String getRandomWord() {
+        return randomWord;
+    }
+
+    public static void setRandomWord(String randomWord) {
+        Words.randomWord = randomWord;
+    }
+
+    public static int getNumLetters() {
+        return numLetters;
+    }
+
+    public static void setNumLetters(int numLetters) {
+        Words.numLetters = numLetters;
+    }
 }
